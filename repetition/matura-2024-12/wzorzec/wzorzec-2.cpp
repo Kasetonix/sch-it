5076d6200d82ba440cf6ba6d67a91e79b579c858
Kasetonix <kasetonix@gmail.com>
2025-03-30 00:04:07 +0100

    [30.03.2025] Finished wzorzec-2.cpp

--- repetition/matura-2024-12/wzorzec/wzorzec-2.cpp
+++ repetition/matura-2024-12/wzorzec/wzorzec-2.cpp
@@ -7,6 +7,8 @@ using std::string;
 using std::ifstream;
 using std::ofstream;
 
+const string INFILE = "szyfrogram.txt";
+const string OUTFILE = "wyniki4.txt";
 const unsigned NUM = 10;
 struct Set {
     unsigned sublen;
@@ -15,19 +17,22 @@ struct Set {
 
 void GetData(Set set[]);
 bool MonoSubtext(string sub);
+bool Repeat(string c_sub, vector<string> sub);
 unsigned SubtextNum(Set set);
+void ReturnData(Set set[]);
 
 int main() {
     Set set[NUM];
     GetData(set);
+    ReturnData(set);
 
     return 0;
 }
 
 void GetData(Set set[]) {
-    ifstream infile;
     unsigned index;
-    infile.open("szyfrogram.txt");
+    ifstream infile;
+    infile.open(INFILE);
     if (!infile.good()) {
         cout << "Plik wejściowy nie mógł być otwarty." << "\n";
         exit(1);
@@ -41,9 +46,11 @@ void GetData(Set set[]) {
 
 // Funkcja sprawdzająca czy podtekst składa się tylko z jednego znaku
 bool MonoSubtext(string sub) {
-    bool mono;
+    bool mono = true;
     unsigned index;
 
+    // Iteracja po całym stringu i sprawdzanie, czy znajdzie się chociaż
+    // jeden znak inny niż poprzedni
     for (index = 1; index < sub.size(); index++) {
         if (sub[index - 1] != sub[index]) {
             mono = false;
@@ -54,42 +61,35 @@ bool MonoSubtext(string sub) {
     return mono;
 }
 
+// Funkcja sprawdzająca czy podtekst znajduje się już w wectorze 
+bool Repeat(string c_sub, vector<string> sub) {
+    unsigned index;
+    bool repeat = false;
+
+    // Iteracja po całym wektorze i sprawdzanie, czy sprawdzany podtekst
+    // już się w nim znajduje
+    for (index = 0; index < sub.size(); index++) {
+        if (sub[index] == c_sub) {
+            repeat = true;
+            break;
+        }
+    }
+    
+    return repeat;
+}
+
+// Funkcja zliczająca unikatowe podteksty
 unsigned SubtextNum(Set set) {
-    unsigned index, v_index, s_index;
-    bool repeat, skip;
+    unsigned index;
     string c_sub;
     vector<string> sub;
-
-    // Pierwszy podtekst
-    sub.push_back(set.str.substr(0, set.sublen));
 
     for (index = 0; index < set.str.size() - set.sublen; index++) {
         c_sub = set.str.substr(index, set.sublen);
 
         // Pomijanie podtekstów złożonych z jednego znaku
-        skip = true;
-        for (s_index = 1; s_index < c_sub.size(); s_index++) {
-            if (c_sub[s_index - 1] != c_sub[s_index]) {
-                skip = false;
-                break;
-            }
-        }
-
-        if (skip)
-            continue;
-
-        // Sprawdzanie, czy podtekst się powtarza
-        // jeżeli repeat == true, to tak
-        repeat = false;
-        for (v_index = 0; v_index < sub.size(); v_index++) {
-            if (sub[v_index] == c_sub) {
-                repeat = true;
-                break;
-            }
-        }
-
-        // Pomijanie powtarzającego się podtekstu
-        if (repeat)
+        // lub już powtarzających się
+        if (MonoSubtext(c_sub) || Repeat(c_sub, sub))
             continue;
 
         sub.push_back(c_sub);
@@ -97,4 +97,21 @@ unsigned SubtextNum(Set set) {
 
     // Liczba unikatowych podtekstów
     return sub.size();
+}
+
+void ReturnData(Set set[]) {
+    unsigned index;
+    ofstream outfile;
+    outfile.open(OUTFILE, std::ios::app);
+    if (!outfile.good()) {
+        cout << "Plik wejściowy nie mógł być otwarty." << "\n";
+        exit(1);
+    }
+
+    outfile << "/// 4.2 ///" << "\n";
+    for (index = 0; index < NUM; index++)
+        outfile << index + 1 << ": " << SubtextNum(set[index]) << "\n";
+    outfile << "\n";
+
+    outfile.close();
 }
